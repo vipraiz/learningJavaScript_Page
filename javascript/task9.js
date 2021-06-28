b48ce42e5f9980252554c8ed120c77460b48dfd0
vipraiz <vipraizhas@gmail.com>
2021-06-29 00:18:07 +0700

    Added task9

--- javascript/task9.js
+++ javascript/task9.js
@@ -1,3 +1,30 @@
+$('.task9-inputs')
+  .children()
+  .children()
+  .on('change', function () {
+    $(this).val((_i, v) => Math.max(this.min, Math.min(this.max, v)));
+  });
+
 $('.task9-btn').on('click', function () {
-  // do smth
+  let X1 = Number($('#task9-x1').val()),
+    Y1 = Number($('#task9-y1').val()),
+    X2 = Number($('#task9-x2').val()),
+    Y2 = Number($('#task9-y2').val());
+
+  let string = 'Телепортация невозможна.';
+  if (X1 != X2 || Y1 != Y2) {
+    if ((X1 + Y1) % 2 == (X2 + Y2) % 2) {
+      if (Math.abs(X1 - X2) == Math.abs(Y1 - Y2)) {
+        string = 1;
+      } else {
+        string = 2;
+      }
+    }
+  }
+
+  Swal.fire({
+    icon: isNaN(string) ? 'error' : 'success',
+    title: 'task9',
+    html: string,
+  });
 });
